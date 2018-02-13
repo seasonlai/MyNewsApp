b2a610b0988591e4e76fccb8c6c96e54afcc8dee
wellhope <710205009@qq.com>
2018-02-13 16:03:04 +0800

    .

---
+++ app/src/main/java/com/example/wellhope/mynewsapp/Student.java
@@ -0,0 +1,16 @@
+package com.example.wellhope.mynewsapp;
+
+/**
+ * Created by Wellhope on 2018/2/12.
+ */
+
+public class Student {
+
+    String name;
+    int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
